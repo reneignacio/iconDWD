5e42a610e5c22596595dd28fe1880babb7994de7
reneignacio <reneignaciosepulvedajara@gmail.com>
2024-03-22 19:50:34 -0300

    add funcion filtrar variable

--- vignette/example.R
+++ vignette/example.R
@@ -2,7 +2,7 @@ library(iconDWD)
 library(glue)
 library(R.utils)
 
-directorioBase <- "E:/ICON/20240319/06"
+directorioBase <- "E:/ICON/20240319/18"
 
 rutas_bz2 <- list.files(directorioBase, pattern = "\\.bz2$", full.names = TRUE, recursive = TRUE)
 extraerBZ2(rutas = rutas_bz2, parallel = TRUE, ncores = 8)
@@ -10,4 +10,12 @@ extraerBZ2(rutas = rutas_bz2, parallel = TRUE, ncores = 8)
 
 rutas_grib2 <- list.files(directorioBase, pattern = "\\.grib2$", full.names = TRUE, recursive = TRUE)
 ruta_script_wsl = "/home/inia/ICON_0125/transform_0125.sh"
-?Grib2ANetCDF(ruta_in = rutas_grib2, parallel = T,verbose = F,ruta_script = ruta_script_wsl)
+Grib2ANetCDF(ruta_in = rutas_grib2, parallel = T,verbose = F,ruta_script = ruta_script_wsl)
+
+extraer_valores_icon
+
+coords_estacion <- obtener_coords_estacion(nombre_estacion = "Ce arroz")
+valores_y_fechas <- extraer_valores_y_fechas(icon_18, coords_estacion)
+print(valores_y_fechas)
+
+
